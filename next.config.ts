538324a033ea330508ee8af32e7db7abb21510b3
kim gwangjin <kkj68106810@gmail.com>
2026-09-23 18:00:28 +0900

    20260923 front-end CORS configuration added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*', // /api 로 시작하는 요청은
+                destination: 'http://localhost:8080/api/:path*', // 백엔드로 우회 전달
+            },
+        ];
+    },
 };
 
 export default nextConfig;
